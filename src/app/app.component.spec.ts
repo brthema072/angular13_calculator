a5ddcf707c4d590c0cc402a3ea6cf7c154ddec99
Bruno Almeida <bruno.almeida@ibm.com>
2022-10-17 18:51:41 -0300

    tsconfig.json e removendo testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'angular-calculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-calculator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(fixture.componentInstance.title).toContain('angular-calculator');
-  });
 });
